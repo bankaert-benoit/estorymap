6163a2daf4e41eab540089161cebe53865a2830f
bankaert <benoit.bankaert.etu@univ-lille.fr>
2022-02-27 18:02:39 +0100

    first commit

---
+++ src/test/java/fr/equipegris/EStorymap/EStorymapApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.equipegris.EStorymap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EStorymapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
