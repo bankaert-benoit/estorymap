480e65e1f477cff1a6f269c21dde0757432d08c0
benjamin-etu <100831890+benjamin-etu@users.noreply.github.com>
2022-03-16 10:11:36 +0100

    Ajout de la sauvegarde du fichier

--- src/main/java/fr/equipegris/EStorymap/file/FileUpload.java
+++ src/main/java/fr/equipegris/EStorymap/file/FileUpload.java
@@ -6,15 +6,47 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 
 @RestController
 public class FileUpload {
+	
+	@Autowired
+	private FileEntityRepository repo;
 
     @PostMapping(value = "/upload")
     public ResponseEntity<?> retrieveFile(@RequestParam("file") MultipartFile req) {
         System.out.println(req);
+        //save file to DB
+        if (req.isEmpty()) {
+        	//TODO: dire au client que son fichier est vide
+        	// et stopper exécution
+        }else {
+        	 this.saveUploadedFileToDB(req);
+        }
         return new ResponseEntity<String>(HttpStatus.OK);
+    }
+    
+    /**
+     * Save an uploaded file to DB
+     * @param file : MultipartFile from a request
+     */
+    public void saveUploadedFileToDB(MultipartFile mpFile) {
+    	try {
+    		String filename = mpFile.getOriginalFilename(); //attention à getOriginalFileName (cf doc)
+			byte[] content = mpFile.getBytes();
+			FileEntity fileEntity = new FileEntity(filename, content);
+			
+	    	repo.save(fileEntity); //on sauvegarde le fichier
+		} catch (IOException e) {
+			//TODO: Montrer l'erreur du côté client
+			e.printStackTrace();
+			System.err.println("Error on get content from uploaded file.");
+		}
+    	
     }
 }
