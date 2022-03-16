cc06232f410edc04096fafb8e8c55262fecdac8c
benjamin-etu <100831890+benjamin-etu@users.noreply.github.com>
2022-03-16 09:26:04 +0100

    Création classe pour représenter un fichier

---
+++ src/main/java/fr/equipegris/EStorymap/file/FileEntity.java
@@ -0,0 +1,59 @@
+
+package fr.equipegris.EStorymap.file;
+
+import javax.persistence.*;
+
+@Entity
+public class FileEntity {
+	
+	@Id
+	@GeneratedValue ( strategy = GenerationType . SEQUENCE , generator =" Seq_file ")
+	@SequenceGenerator ( name =" Seq_file ", sequenceName =" Seq_file ", allocationSize =1)
+	private Long id_file;
+	
+	private String filename;
+	private byte[] content;
+	
+	/**
+	 * Creation of a FileEntity
+	 * @param filename name of file
+	 * @param content content of file
+	 */
+	public FileEntity(String filename, byte[] content) {
+		this.filename = filename;
+		this.content = content;
+	}
+	
+	/**
+	 * 
+	 * @return name of this file
+	 */
+	public String getFilename() {
+		return this.filename;
+	}
+	
+	/**
+	 * Set a new name for this file
+	 * @param name
+	 */
+	public void setFilename(String name) {
+		this.filename = name;
+	}
+	
+	/**
+	 * 
+	 * @return content of this file
+	 */
+	public byte[] getContent() {
+		return this.content;
+	}
+	
+	/**
+	 * Set new content for this file.
+	 * Overwrite current content.
+	 * @param newContent to overwrite
+	 */
+	public void setContent(byte[] newContent) {
+		this.content = newContent;
+	}
+}
